54d58729a27de382383fff0742fb43e7dfe0ccd9
Petrus Rennan <petrus@i9me.com.br>
2018-07-13 13:20:50 -0300

    segundo commit

--- template-parts/institucional/section-institucional.php
+++ template-parts/institucional/section-institucional.php
@@ -1,36 +1,50 @@
-<section class="Section Section--institucional u-paddingHorizontal u-displayFlex">
-	<div class="Section-subSection Section-subSection--sobre u-size12of24">
+<section class="Section--institucional u-paddingHorizontal u-flexFlowWrap u-displayFlex">
+	<div class="Section-subSection--sobre u-displayFlex u-flexDirectionColumn u-paddingHorizontal--vrt--inter--px u-size12of24">
 		<header class="Section-subSection-header">
-			<h3 class="Section-subSection-header-title Section-subSection-header-title--beforeTitleLine">Sobre</h3>
+			<h3 class="Section-subSection-header-title">Sobre</h3>
 		</header>
 		<div class="Section-subSection-content">
 			<figure class="Section-subSection-items-item-figure">
-				<img class="u-objectFitCover u-height5of10 u-sizeFull" src="<?php echo get_template_directory_uri() ?>/assets/images/modapraia.png">
+				<img class="u-objectFitCover u-height5of10 u-sizeFull u-borderRadius10" src="<?php echo get_template_directory_uri() ?>/assets/images/modapraia.png">
 			</figure>
-			<p class="Section-subSection-items-item-resume">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit auctor eros, sit amet gravida purus convallis sit amet. Fusce urna est, tempor quis aliquam sit amet, dapibus id nulla. Quisque nibh nulla, eleifend a.</p>
+			<p class="Section-subSection-items-item-resume u-paddingHorizontal--inter">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit auctor eros, sit amet gravida purus convallis sit amet. Fusce urna est, tempor quis aliquam sit amet, dapibus id nulla. Quisque nibh nulla, eleifend a.</p>
 			<a href="#" class="Button Button--border Button-background">Ver mais</a>
 		</div>
 	</div>
-	<div class="Section-subSection--faleConosco u-size12of24">
+	<div class="Section-subSection--faleConosco u-size12of24 u-paddingHorizontal--vrt--inter--px">
 		<header class="Section-subSection-header">
 			<h3 class="Section-subSection-header-title Section-subSection-header-title--beforeTitleLine">Fale Conosco</h3>
 		</header>
 		<div class="Section-subSection-content">
-			<p class="Section-subSection-items-item-resume">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit auctor eros, sit amet gravida purus convallis sit amet.</p>
+			<p class="Section-subSection-items-item-resume u-paddingHorizontal--inter">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit auctor eros, sit amet gravida purus convallis sit amet.</p>
 			<form class="Form Form--style1">
 				<fieldset class="Form-fieldset">
-					 <div class="Form-row u-size24of24 u-marginBottom--inter--half u-overflowHidden u-positionRelative u-displayFlex is-animating u-marginRight">
+				 	<div class="Form-line u-size24of24 u-marginBottom--inter--half u-overflowHidden u-positionRelative u-displayFlex is-animating u-marginRight">
+					 	<div class="Form-row u-size12of24 u-marginBottom--inter--half u-overflowHidden u-positionRelative u-displayBlock is-animating u-marginRight--inter">
 							<input type="text" name="name" placeholder="NOME" class="u-borderRadius5 Form-input Form-input--text Form-border--0 u-size24of24">
+						</div>
+
+						<div class="Form-row u-size12of24 u-marginBottom--inter--half u-overflowHidden u-positionRelative u-displayBlock is-animating u-marginRight--inter">
 							<input type="tel" name="telefone" placeholder="TELEFONE" class="u-borderRadius5 Form-input Form-input--text Form-border--0 u-size24of24">
 				        </div>
-				        <div class="Form-row u-size24of24 u-marginBottom--inter--half u-overflowHidden u-positionRelative u-displayFlex is-animating u-marginRight">
+
+			      	</div>
+
+			        <div class="Form-line u-size24of24 u-marginBottom--inter--half u-overflowHidden u-positionRelative u-displayFlex is-animating u-marginRight">
+			        	<div class="Form-row u-size12of24 u-marginBottom--inter--half u-overflowHidden u-positionRelative u-displayBlock is-animating u-marginRight--inter">
 							<input type="text" name="mail" placeholder="E-MAIL" class="u-borderRadius5 Form-input Form-input--text Form-border--0 u-size24of24">
+						</div>
+						<div class="Form-row u-size12of24 u-marginBottom--inter--half u-overflowHidden u-positionRelative u-displayBlock is-animating u-marginRight--inter">
 							<input type="text" name="assunto" placeholder="ASSUNTO" class="u-borderRadius5 Form-input Form-input--text Form-border--0 u-size24of24">
 				        </div>
-				        <div class="Form-row u-size24of24 u-marginBottom--inter--half u-overflowHidden u-positionRelative u-displayFlex is-animating u-marginRight">
-							<textarea name="mensagem" placeholder="MENSAGEM" class="u-borderRadius5 Form-input Form-input--text Form-border--0 u-size24of24"></textarea>
-				        </div>
-				        <div class="Form-row u-size24of24 u-marginBottom--inter--half u-overflowHidden u-positionRelative u-displayFlex is-animating u-marginRight">
+				    </div>
+		      		<div class="Form-line u-size24of24 u-marginBottom--inter--half u-overflowHidden u-positionRelative u-displayFlex is-animating u-marginRight">
+		      			<div class="Form-row u-size24of24 u-marginBottom--inter--half u-overflowHidden u-positionRelative u-displayBlock is-animating u-marginRight--inter u-height5of10">
+							<textarea name="mensagem" placeholder="MENSAGEM" class="u-borderRadius5 Form-input Form-input--textarea Form-border--0 u-size24of24"></textarea>
+						</div>
+			       	</div>
+				        <div class="Form-line u-size24of24 u-marginBottom--inter--half u-overflowHidden u-positionRelative u-displayFlex is-animating u-marginRight">
+				        	<div class="Form-row u-size12of24 u-marginBottom--inter--half u-overflowHidden u-positionRelative u-displayBlock is-animating u-marginRight--inter">
 							<input type="submit" id="_form_13_submit" class="Button Button--border Button--background ButtonLink u-displayInlineFlex u-flexDirectionRow u-flexAlignItemsCenter u-justifyContentCenter u-borderRadius5 Button--mediumSize hover is-animating u-hasIcon u-isScrollDown u-sizeFull" value="CADASTRAR" style="height: 75px;">
 				        </div>
 
