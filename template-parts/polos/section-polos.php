bcdd7f53927f69f96e330a547629a513092c78d1
DIOGO SOARES <diogo@i9me.com.br>
2018-08-17 10:27:32 -0300

    sections polos

--- template-parts/polos/section-polos.php
+++ template-parts/polos/section-polos.php
@@ -7,37 +7,37 @@
 	<div class="Section-content u-flex u-flexAlignItemsCenter u-sizeFull u-paddingVertical">
 		<ul id="polos-carousel" class="Section-items u-flex u-flexJustifyContentCenter u-flexDirectionRow u-flexAlignItemsCenter u-sizeFull owl-carousel owl-theme u-sizeFull">
 			<li class="Section-items-item SaoPaulo u-marginVertical--inter u-sizeFull">
-					<a href="#" class="Section-items-item-content u-displayFlex u-flexJustifyContentCenter u-flexDirectionColumn u-flexAlignItemsCenter">
+					<a href="<?php echo get_home_url(); ?>/centros-de-moda/sao-paulo/" class="Section-items-item-content u-displayFlex u-flexJustifyContentCenter u-flexDirectionColumn u-flexAlignItemsCenter">
 						<i class="FigureIcon FigureIcon--masp"></i>
 						<h3 class="Section-items-item-title u-paddingTop--inter--half">São Paulo - SP</h3>
 				    </a>
 			</li>
 			<li class="Section-items-item SaoPaulo u-marginVertical--inter u-sizeFull">
-				<a href="#" class="Section-items-item-content u-displayFlex u-flexJustifyContentCenter u-flexDirectionColumn u-flexAlignItemsCenter">
+				<a href="<?php echo get_home_url(); ?>/centros-de-moda/fortaleza/belo-horizonte" class="Section-items-item-content u-displayFlex u-flexJustifyContentCenter u-flexDirectionColumn u-flexAlignItemsCenter">
 					<i class="FigureIcon FigureIcon--bh"></i>
 					<h3 class="Section-items-item-title u-paddingTop--inter--half">Belo Horizonte - MG</h3>
 			    </a>
 			</li>
 			<li class="Section-items-item Maringa u-marginVertical--inter u-sizeFull">
-				<a href="#" class="Section-items-item-content u-displayFlex u-flexJustifyContentCenter u-flexDirectionColumn u-flexAlignItemsCenter">
+				<a href="<?php echo get_home_url(); ?>/centros-de-moda/maringa/" class="Section-items-item-content u-displayFlex u-flexJustifyContentCenter u-flexDirectionColumn u-flexAlignItemsCenter">
 					<i class="FigureIcon FigureIcon--maringa"></i>
 					<h3 class="Section-items-item-title u-paddingTop--inter--half">Maringá - PR</h3>
 			    </a>
 			</li>
 			<li class="Section-items-item Goiania u-marginVertical--inter u-sizeFull">
-				<a href="#" class="Section-items-item-content u-displayFlex u-flexJustifyContentCenter u-flexDirectionColumn u-flexAlignItemsCenter">
+				<a href="<?php echo get_home_url(); ?>/centros-de-moda/goiania/" class="Section-items-item-content u-displayFlex u-flexJustifyContentCenter u-flexDirectionColumn u-flexAlignItemsCenter">
 					<i class="FigureIcon FigureIcon--goiania"></i>
 					<h3 class="Section-items-item-title u-paddingTop--inter--half">Goiânia - GO</h3>
 			    </a>
 			</li>
 			<li class="Section-items-item Fortaleza u-marginVertical--inter u-sizeFull">
-				<a href="#" class="Section-items-item-content u-displayFlex u-flexJustifyContentCenter u-flexDirectionColumn u-flexAlignItemsCenter">
+				<a href="<?php echo get_home_url(); ?>/centros-de-moda/fortaleza/" class="Section-items-item-content u-displayFlex u-flexJustifyContentCenter u-flexDirectionColumn u-flexAlignItemsCenter">
 					<i class="FigureIcon FigureIcon--fortaleza"></i>
 					<h3 class="Section-items-item-title u-paddingTop--inter--half">Fortaleza - CE</h3>
 			    </a>
 			</li>
 			<li class="Section-items-item Caruaru u-marginVertical--inter u-sizeFull">
-				<a href="#" class="Section-items-item-content u-displayFlex u-flexJustifyContentCenter u-flexDirectionColumn u-flexAlignItemsCenter">
+				<a href="<?php echo get_home_url(); ?>/centros-de-moda/caruraru/" class="Section-items-item-content u-displayFlex u-flexJustifyContentCenter u-flexDirectionColumn u-flexAlignItemsCenter">
 					<i class="FigureIcon FigureIcon--caruaru"></i>
 					<h3 class="Section-items-item-title u-paddingTop--inter--half">Caruaru - PE</h3>
 			    </a>
